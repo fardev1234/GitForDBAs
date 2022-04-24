d8c161c69af85c22b28b75f2a6c7dfc8d96478e7
fardev1234 <farhana.s.reid@occc.edu>
2022-04-24 11:49:06 -0500

    comiting v2.5

--- migrations/V2.5__Test.sql
+++ migrations/V2.5__Test.sql
@@ -2,4 +2,4 @@ INSERT dbo.ILikeDags (Declaration)
 VALUES 
     ('I also like dags test'),
     ('I think this is a fight club reference test'),
-    ('You can step in the same river twice test');
+    ('You can step in the same river twice tek;lkklst');
